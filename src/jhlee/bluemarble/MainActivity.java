b60ee3685132e92a2034e5ada9e7dbca843c8cdf
Lee junghyun <boom1492@nate.com>
2012-06-14 01:59:58 +0900

    join / leave / ready / dice / buy 구현완료

--- src/jhlee/bluemarble/MainActivity.java
+++ src/jhlee/bluemarble/MainActivity.java
@@ -1,29 +1,67 @@
 package jhlee.bluemarble;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
-    @Override
+	private final static int REQUEST_CODE = 0;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Button but = (Button)findViewById(R.id.button_startID);
-        but.setOnClickListener(new OnClickListener() {
+        final EditText text_id = (EditText)findViewById(R.id.text_ID);
+        Button but_id = (Button)findViewById(R.id.button_startID);
+        but_id.setOnClickListener(new OnClickListener() {
 			
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				setContentView(R.layout.test);
-		        
+				Intent intent = new Intent(MainActivity.this, JoinActivity.class);
+				Bundle bundle = new Bundle();
+				bundle.putString("mCode", text_id.getText().toString());
+				intent.putExtras(bundle);
+				startActivity(intent);
+			}
+		}); 
+        Button but_qr = (Button)findViewById(R.id.button_startQR);
+        but_qr.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+				startActivityForResult(intent, REQUEST_CODE);
 			}
 		});
-        Toast.makeText(this, "테스트", Toast.LENGTH_LONG).show();
-        
     }
+	
+    @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode==REQUEST_CODE){
+			if(resultCode == RESULT_OK){
+				Uri uri = Uri.parse(data.getStringExtra("SCAN_RESULT"));
+				
+				String mCode = uri.getQueryParameter("sessionId");
+
+				Intent intent = new Intent(MainActivity.this, JoinActivity.class);
+				Bundle bundle = new Bundle();
+				bundle.putString("mCode", mCode);
+				intent.putExtras(bundle);
+				startActivity(intent);
+			}
+			else if(resultCode == RESULT_CANCELED){
+				// QR코드 스캐너에서 취소할 경우 핸들링
+			}
+		}
+
+	}
 }
